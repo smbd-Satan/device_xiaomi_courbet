2e5c7be4706295e8736d49918fb1068c9994522e
basamaryan <basam.aryan@gmail.com>
2023-11-04 20:19:56 +0100

    sm6150-common: Use the common excluded-input-devices.xml
    Change-Id: I22da0119acb1df87eb093ab311fa86369817ca8a

--- device.mk
+++ device.mk
@@ -495,7 +495,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+    vendor/qcom/opensource/vibratorexcluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Vulkan
 PRODUCT_PACKAGES += \
